fac3e97bcf99a637ec227342d54ee2f9e77b2d54
Glitch (guttural-galliform) <none>
2019-05-11 14:23:11 +0000

    Working out why routing is not working...

--- routes/api.js
+++ routes/api.js
@@ -13,6 +13,8 @@ var MongoClient = require('mongodb').MongoClient;
 var ObjectId = require('mongodb').ObjectId;
 const MONGODB_CONNECTION_STRING = process.env.DB;
 //Example connection: MongoClient.connect(MONGODB_CONNECTION_STRING, function(err, db) {});
+var LibraryHandler = require('../controllers/libraryHandler.js');
+const COLLECTION = "Book";
 
 module.exports = function (app) {
 
@@ -21,14 +23,25 @@ module.exports = function (app) {
   
     if(err) console.log('Database error: ' + err);
     
+    console.log("New DB Connection");
+    LibraryHandler = new LibraryHandler(app,db);
+    console.log("New Library Handler");
+    
     app.route('/api/books')
       .get(function (req, res){
+        console.log("Got Here 2");
+        
         //response will be array of book objects
         //json res format: [{"_id": bookid, "title": book_title, "commentcount": num_of_comments },...]
+        
       })
 
       .post(function (req, res){
+        console.log("Got Here");
         var title = req.body.title;
+        LibraryHandler.addBook(COLLECTION, title)
+        .then((result)=>{console.log(result);res.json(result);})
+        .catch((reject)=>{res.json(reject);}); 
         //response will contain new book object including atleast _id and title
       })
 
